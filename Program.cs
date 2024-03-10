20d6cd20212ab1abef77b58328d3a41e73f8315d
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2024-03-10 19:19:17 +0000

    add homework

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿bool operand1Required = true;
+float operand1Value = 0;
+
+while (operand1Required)
+{
+    Console.WriteLine("Enter the first operand: ");
+    string operand1 = Console.ReadLine();
+    bool oprrand1IsNumber = float.TryParse(operand1, out operand1Value);
+    if (oprrand1IsNumber == false)
+    {
+        Console.WriteLine("Invalid input. Please enter a number.");
+    }
+    else
+    {
+        operand1Required = false;
+    }
+}
+
+bool operatorRequired = true;
+string operatorValue = "";
+
+while (operatorRequired)
+{
+    Console.WriteLine("Enter the operator: ");
+    operatorValue = Console.ReadLine();
+    if (operatorValue == "+" || operatorValue == "-" || operatorValue == "*" || operatorValue == "/")
+    {
+        operatorRequired = false;
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a valid operator (+, -, *, /).");
+    }
+}
+
+bool operand2Required = true;
+float operand2Value = 0;
+
+while (operand2Required)
+{
+    Console.WriteLine("Enter the second operand: ");
+    string operand2 = Console.ReadLine();
+    bool oprrand2IsNumber = float.TryParse(operand2, out operand2Value);
+    if (oprrand2IsNumber == false)
+    {
+        Console.WriteLine("Invalid input. Please enter a number.");
+    }
+    else
+    {
+        operand2Required = false;
+    }
+}
+
+float result = 0;
+
+// Calculate the result
+// continue from here
+
+Console.WriteLine("The result is: " + result);
---
+++ readme.md
@@ -0,0 +1,15 @@
+## Homework 2
+
+### 1. Introduction
+
+This homework is about branches and loops. We will use the `if` statement to control the flow of the program and the `for` and `while` loops to repeat the execution of a block of code.
+
+### 2. Objectives
+
+You are given a simple program that implements 4 main mathematical operations: addition, subtraction, multiplication, and division. The program is incomplete and you need to complete it. The program should ask the user to enter two numbers and then ask the user to choose an operation. The program should then perform the chosen operation and display the result.
+
+- Learn that any input to the program must be validated.
+- To understand the `if` statement and its usage.
+- To understand the `while` loops and their usage.
+- Complete the program.
+- Test the program.
